54006dae0b870d63bce999c4c0d9f5bd390f16d7
arke19 <arke19me@student.ju.se>
2022-10-31 19:51:36 +0100

    refactor variable

--- Labb7/labb7.py
+++ Labb7/labb7.py
@@ -7,9 +7,9 @@ sockL.listen(1)
 listOfSockets = [sockL]
 print("Listening on port {}".format(port))
 def send_message(addr, message):
-    for sockets in listOfSockets:
-        if sockets != sockL:
-            sockets.sendall(bytearray('Client: {} {}'.format(addr, message), 'ascii'))
+    for client in listOfSockets:
+        if client != sockL:
+            client.sendall(bytearray('Client: {} {}'.format(addr, message), 'ascii'))
 while True:
     tup = select.select(listOfSockets, [], [])
     sock = tup[0][0]
